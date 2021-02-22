2cab373791b3ec4a62a0aa42c7f893f987a2c52c
farmandev <mdali.lioncoders@gmail.com>
2021-02-22 10:03:26 +0600

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,43 +25,35 @@
 
     $('.notifi-icon').on('click', function() {
         $(this).toggleClass('open');
-        // $('.body_overlay').toggleClass('open');
+        $('.body_overlay').toggleClass('open');
     });
     $('.client-menu').on('click', function() {
-        // $('.body_overlay').toggleClass('open');
+        $('.body_overlay').toggleClass('open');
         $(this).toggleClass('open');
     });
-    // $('.pop-btn').on('click', function() {
-    //     $('.home').addClass('open');
-    //     // $('.create-mission-wrap').addClass('d-none');
-    //     // $('.filter-btn-wrap').removeClass('d-none');
-    //     // $('.filter-btn-wrap').addClass('d-flex');
-    //     // $('.mission-slider-wrap.v1').removeClass('d-none');
-    // });
-    //popup close
-    // $('.pop-btn').on('click', function() {
-    //     if ($('.mission-slider-wrap.v1').hasClass('d-none')) {
-    //         $('.mission-slider-wrap.v1').removeClass('d-none');
-    //         $('.create-mission-wrap').addClass('d-none');
-    //     }
-    // });
-    // $('.body_overlay').on('click', function() {
-    //     if($('.client-menu').hasClass('open') && $('.notif-list').hasClass('open')){
-    //         $('.client-menu').removeClass('open');
-    //         $('.notif-list').removeClass('open');
-    //     }
+     $('.body_overlay').on('click', function() {
+        $(this).removeClass('open');
+        if($('.client-menu').hasClass('open')){
+            $('.client-menu').removeClass('open');
+        }
+        if($('.notifi-icon').hasClass('open')){
+            $('.notifi-icon').removeClass('open');
+        }
+    });
 
-    // });
+    
 
     //Slider 
 
     $('.mission-slider-one').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        dots: true,
+        dots: false,
         infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
         arrows: false,
-        speed: 300,
+        speed:900,
         centerMode: false,
         // slidesToShow: 1,
         // slidesToScroll: 1,
@@ -86,7 +78,8 @@
             {
                 breakpoint: 767,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    dots: true
                 }
             }
 
@@ -96,12 +89,12 @@
         slidesToShow: 4,
         slidesToScroll: 1,
         dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         arrows: false,
-        speed: 300,
+        speed: 900,
         centerMode: false,
-        // slidesToShow: 1,
-        // slidesToScroll: 1,
         responsive: [{
                 breakpoint: 1440,
                 settings: {
@@ -123,7 +116,8 @@
             {
                 breakpoint: 767,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    dots: true
                 }
             }
 
@@ -151,24 +145,12 @@
 
     /* ----------------------------------------
                   datepicker
-           ------------------------------------------- */
+     ------------------------------------------- */
     $("#datepicker-from-1, #datepicker-to-1").datepicker({
         autoclose: true,
         todayHighlight: true
     });
     $('select').niceSelect();
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 })(jQuery);
